199ed9f702a1a8ae03101190998e199e4896f504
Mark Adesina Omoniyi <developerslearnit@gmail.com>
2017-02-07 13:29:33 +0100

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
     'serverPort':5000,
-    'dbUrl':'mongodb://dbuser:dbpassword@ds143737.mlab.com:43737/photobooth',
-    'cloud_name': 'panachora',
-    'api_key': '745522158985413',
-    'api_secret': '9sl4svF78H0Xkkk-fHT5suDi4o8'
+    'dbUrl':'url-to-your-mongodb',
+    'cloud_name': 'cloudinary-cloud-name',
+    'api_key': 'cloudinary-api-key',
+    'api_secret': 'cloudinary-api-secret'
 }
